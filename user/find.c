983548e4e7f01eb3b2414649fb272518aa2adaae
lhf <978339710@qq.com>
2023-02-19 09:04:26 -0800

    finish untill "find"

---
+++ user/find.c
@@ -0,0 +1,67 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+#include "kernel/fs.h"
+
+void find(char* dir, char* filename) {
+    char buff[512], *p;
+    int fd;
+    struct dirent de;
+    struct stat st;
+
+    if ((fd = open(dir, 0)) < 0) {
+        fprintf(2, "cant open %s\n", dir);
+        return;
+    }
+
+    if (fstat(fd, &st) < 0) {
+        fprintf(2, "cant stat %s\n", dir);
+        close(fd);
+        return;
+    }
+
+    // printf("23\n");
+
+    switch (st.type) {
+        case T_FILE:
+            // printf("FILE\n");
+            p = dir + strlen(dir) - 1;
+            while (*p != '/') p--;
+
+            if (strcmp(p + 1, filename) == 0) {
+                printf("%s\n", dir);
+            }
+            close(fd);
+            break;
+
+        case T_DIR:
+            // printf("DIR\n");
+            strcpy(buff, dir);
+            p = buff + strlen(buff);
+            *p++ = '/';
+            while (read(fd, &de, sizeof(de))) {
+                if (de.inum == 0 || strcmp(de.name, ".") == 0 || strcmp(de.name, "..") == 0)
+                    continue;
+
+                memmove(p, de.name, sizeof(de.name));
+                buff[strlen(buff)] = '\0';
+                // printf("%s\n", de.name);
+                // printf("de.inum = %d\n", de.inum);
+                // printf("%s\n", buff);
+                find(buff, filename);
+            }
+            close(fd);
+        break;
+    }
+
+    // printf("49\n");
+    return;
+}
+
+int main(int argc, char* argv[]) {
+    if (argc == 3) {
+        find(argv[1], argv[2]);
+    }
+
+    exit(0);
+}
--- user/primes.c
+++ user/primes.c
@@ -1,5 +1,65 @@
+#include "kernel/types.h"
+#include "user/user.h"
 
+void newProcess(int p[]);
 
 int main(int argc, char* argv[]) {
     
+    int p[2];
+    pipe(p);
+    
+    int id = fork();
+    if (id > 0) {
+        close(p[0]);
+        for (int i = 2; i <= 35; i++) {
+            write(p[1], &i, 4);
+        }
+        close(p[1]);
+        wait((int *) 0);
+    }
+    else {
+        newProcess(p);
+
+
+    }
+
+    exit(0);
+}
+
+void newProcess(int p[]) {
+    close(p[1]);
+
+    int first;
+    if (read(p[0], &first, 4)) {
+        // printf("%d\n", getpid());
+        printf("prime %d\n", first);
+    }
+    else {
+        exit(0);
+    }
+
+    int next;
+    int p_next[2];
+    pipe(p_next);
+
+    int id = fork();
+
+    if (id > 0) {
+        close(p_next[0]);
+        while (read(p[0], &next, 4)) {
+            if (next % first != 0) {
+                write(p_next[1], &next, 4);
+            }
+        }
+        close(p_next[1]);
+        wait((int *) 0);
+    }
+    else {
+        newProcess(p_next);
+    }
+
+    exit(0);
+
+    
+
 }
